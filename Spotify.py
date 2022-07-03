633aa1f147572af97c87ba7f73a9eabe239069f3
Kai-R <555kaireffert@gmail.com>
2022-07-03 11:17:08 +0200

    m

--- Spotify.py
+++ Spotify.py
@@ -1,4 +1,12 @@
 import requests
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+
+print(CLIENT_ID)
 response = requests.get("https://api.thedogapi.com/")
 print(response.text)
