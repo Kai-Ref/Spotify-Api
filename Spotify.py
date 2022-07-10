3bdabcaf00fe2bb0275042ccdd1cf43195f2e38f
Kai-R <555kaireffert@gmail.com>
2022-07-10 12:35:09 +0200

    m

--- Spotify.py
+++ Spotify.py
@@ -18,6 +18,66 @@ AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 #Client Credentials Flow
 class Spotify():
+
+
+
+    def dff(self,playlist_id):
+        spotify=self.spotify
+        results = spotify.playlist(playlist_id)
+        # create a list of song ids
+        ids=[]
+
+        for item in results['tracks']['items']:
+                track = item['track']['id']
+                ids.append(track)
+                
+        song_meta={'id':[],'album':[], 'name':[], 
+                'artist':[],'explicit':[],'popularity':[]}
+
+        for song_id in ids:
+            # get song's meta data
+            meta = spotify.track(song_id)
+            
+            # song id
+            song_meta['id'].append(song_id)
+
+            # album name
+            album=meta['album']['name']
+            song_meta['album']+=[album]
+
+            # song name
+            song=meta['name']
+            song_meta['name']+=[song]
+            
+            # artists name
+            s = ', '
+            artist=s.join([singer_name['name'] for singer_name in meta['artists']])
+            song_meta['artist']+=[artist]
+            
+            # explicit: lyrics could be considered offensive or unsuitable for children
+            explicit=meta['explicit']
+            song_meta['explicit'].append(explicit)
+            
+            # song popularity
+            popularity=meta['popularity']
+            song_meta['popularity'].append(popularity)
+
+        song_meta_df=pd.DataFrame.from_dict(song_meta)
+
+        # check the song feature
+        features = spotify.audio_features(song_meta['id'])
+        # change dictionary to dataframe
+        features_df=pd.DataFrame.from_dict(features)
+
+        # convert milliseconds to mins
+        # duration_ms: The duration of the track in milliseconds.
+        # 1 minute = 60 seconds = 60 × 1000 milliseconds = 60,000 ms
+        features_df['duration_ms']=features_df['duration_ms']/60000
+
+        # combine two dataframe
+        final_df=song_meta_df.merge(features_df) 
+        print(final_df)
+        return final_df
     def __init__(self):
         
         self.spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
@@ -26,16 +86,21 @@ class Spotify():
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
             })
+        
 
-        self.spotify.current_playback()
         #convert the response to JSON
         auth_response_data = auth_response.json()
 
         #save the access token
         access_token = auth_response_data['access_token']
         self.headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
-
-
+        id_dict={'2022':'spotify:playlist:0vXaFFlNzilqtZFXx97G02',
+                'HH':'spotify:playlist:0dKeeXOvnBumSstEvUt2tV'}
+        
+        for id in id_dict.values():
+            df=self.dff(playlist_id=id)
+        print(df.columns)
+        
         tracks=[]
         for i in range(len(id)):
             track=getTrackFeatures(self.spotify,id[i])
